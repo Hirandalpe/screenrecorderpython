a1ff0d8f60248939862664c1426a8dfa0a66aabe
Hirandalpe <65526913+Hirandalpe@users.noreply.github.com>
2021-10-08 12:24:39 +0530

    height and width update

--- python_recorder/main.py
+++ python_recorder/main.py
@@ -1,9 +1,13 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2
+from win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
 
 while True :
-    image = ImageGrab.grab(bbox=(0, 0, 1360, 780))
+    image = ImageGrab.grab(bbox=(0, 0, width, height))
     np_array = np.array(image)
     color_correct = cv2.cvtColor(np_array, cv2.COLOR_BGR2RGB)
     cv2.imshow('recorder', color_correct)
